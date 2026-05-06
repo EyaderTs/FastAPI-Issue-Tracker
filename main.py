6cb89393451b5b7041bc65b5a7a23a2341db4b16
Eyader Tsehayu <eyadertsehayu@gmail.com>
2026-05-06 14:06:29 +0300

    arrange the initial routing

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 from fastapi import FastAPI
+from app.routes.issues import router as issues_router
 
 app = FastAPI()
-items =[{"id": 1, "name": "Item 1"}, {"id": 2, "name": "Item 2"}, {"id": 3, "name": "Item 3"},]
+app.include_router(issues_router)
 
 @app.get("/health")
 def health_check():
     return {"status": "Ok"}
-
-@app.get("/items")
-def get_items():
-    return items
-
-@app.get("/items/{item_id}")
-def get_item(item_id: int):
-    for item in items:
-        if item["id"] == item_id:
-            return item
-    return {"error": "Item not found"}
-
-@app.post("/create-items")
-def create_item(item: dict):
-    items.append(item)
-    return item
 
