3e8765d5af59bdf07bb258de022308197c3549d1
Eyader Tsehayu <eyadertsehayu@gmail.com>
2026-05-06 22:21:23 +0300

    add crud routes of issues

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -1,6 +1,72 @@
-from fastapi import APIRouter
+import uuid
+from fastapi import APIRouter, HTTPException, status
+from app.schemas import IssueCreate, IssueUpdate, IssueOut
+from app.storage import load_data, save_data
+
 router = APIRouter(prefix="/api/v1/issues", tags=["issues"])
 
-@router.get("/")
+@router.get("/get-issues", response_model=list[IssueOut])
 def get_issues():
-    return []
+    """Get all issues."""
+    issues = load_data()
+    return issues
+
+@router.get("/get-issue/{issue_id}", response_model=IssueOut)
+def get_issue(issue_id: str):
+    """Get a single issue by ID.
+       Raises HTTPException: If the issue is not found, raises a 404 error.
+    """
+    issues = load_data()
+    for issue in issues:
+        if issue["id"] == issue_id:
+            return issue
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Issue not found")
+
+@router.post("/create-issue", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
+def create_issue(payload: IssueCreate):
+    """Create a new issue."""
+    issues = load_data()
+    issue ={
+        "id": str(uuid.uuid4()),
+        "title": payload.title,
+        "description": payload.description,
+        "priority": payload.priority.value,
+        "status": "open"
+    }
+    issues.append(issue)
+    save_data(issues)
+    return issue
+
+@router.put("/update-issue/{issue_id}", response_model=IssueOut)
+def update_issue(issue_id: str, payload: IssueUpdate):
+    """Update an existing issue by ID.
+       Raises HTTPException: If the issue is not found, raises a 404 error.
+    """
+    issues = load_data()
+    for issue in issues:
+        if issue["id"] == issue_id:
+            if payload.title is not None:
+                issue["title"] = payload.title
+            if payload.description is not None:
+                issue["description"] = payload.description
+            if payload.priority is not None:
+                issue["priority"] = payload.priority.value
+            if payload.status is not None:
+                issue["status"] = payload.status.value
+            
+            save_data(issues)
+            return issue
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Issue not found")
+
+@router.delete("/delete-issue/{issue_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_issue(issue_id: str):
+    """Delete an issue by ID.
+       Raises HTTPException: If the issue is not found, raises a 404 error.
+    """
+    issues = load_data()
+    for i, issue in enumerate(issues):
+        if issue["id"] == issue_id:
+            issues.pop(i)
+            save_data(issues)
+            return
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Issue not found")
